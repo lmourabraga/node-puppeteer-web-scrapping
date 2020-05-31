cda03352b4857b86b41d1787186f4cdc6cde6c45
Luã Braga <ldemourabraga@gmail.com>
2020-05-30 21:15:49 -0300

    Setting the page resolution with setViewport

--- 1-basic-usage/save-screenshot-image.js
+++ 1-basic-usage/save-screenshot-image.js
@@ -5,10 +5,18 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('http://google.com/');
+
+    //Defines a page resolution for the screenshot
+    await page.setViewport({ width: 1920, height: 1080 });
+
+    let url = 'http://google.com/'
+    await page.goto(url);
 
     await page.screenshot({
         path: 'screenshot.png'
     });
+
+    console.log(`The screenshot for the URL: ${url} was done`);
+
     await browser.close();
 })();
